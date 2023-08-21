ab2e924df3696069fa5cf1d95604bbd24bb9e2be
Mayank <mk4664348@gmail.com>
2023-08-22 00:51:54 +0530

    Finished with Navbar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {headerLogo} from '../assets/images'
 import {hamburger} from '../assets/icons'
+import {navLinks} from '../constants'
 
 const Nav = () => {
   return (
@@ -15,8 +16,26 @@ const Nav = () => {
             </a>
 
             <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
-                
+                {navLinks.map((item)=>(
+                    <li key={item.label}>
+                        <a 
+                            href={item.href}
+                            className='font-montserrat leading-normal text-lg text-slate-gray'
+                        >
+                        {item.label}
+                        </a>
+                    </li>
+                ))}
             </ul>
+
+            <div className='hidden max-lg:block duration-550'>
+                <img 
+                    src={hamburger} 
+                    alt="hamburger"
+                    width={25}
+                    height={25} 
+                />
+            </div>
         </nav>
     </header>
   )
